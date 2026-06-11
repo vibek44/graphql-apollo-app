4285effaa28962d41c33c871b3cd0bb238fb433f
vibek44 <kshetrivibek@gmail.com>
2026-06-11 21:02:47 +0300

    adding feature to see book  view according to genre

--- library-frontend/src/components/Books.jsx
+++ library-frontend/src/components/Books.jsx
@@ -1,17 +1,40 @@
+import { useState } from "react";
 import { ALL_BOOK } from "../queries";
 import { useQuery } from "@apollo/client/react";
 const Books = (props) => {
-  const { error, loading, data } = useQuery(ALL_BOOK);
+  const [search, setSearch] = useState(null);
+  const genre = search === null || search === "allgenres" ? undefined : search;
+  const { error, loading, data } = useQuery(ALL_BOOK, {
+    variables: { genre },
+  });
+  /*const filteredGenres =
+    data &&
+    data.allBook.length > 0 &&
+    (search === "allgenres"
+      ? data.allBook
+      : data.allBook.filter((book) => book.genres.includes(search)));
+*/
+  const genres = [
+    "refactoring",
+    "agile",
+    "patterns",
+    "design",
+    "crime",
+    "allgenres",
+  ];
+
   if (!props.show) {
     return null;
   }
 
   if (loading) return <h3>Loading...</h3>;
   if (error) return <h3>Error connection</h3>;
-
   return (
     <div>
       <h2>books</h2>
+      <p>
+        in genre <b>{search}</b>
+      </p>
 
       <table>
         <tbody>
@@ -29,6 +52,11 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      {genres.map((genre) => (
+        <button key={genre} onClick={() => setSearch(genre)}>
+          {genre}
+        </button>
+      ))}
     </div>
   );
 };
