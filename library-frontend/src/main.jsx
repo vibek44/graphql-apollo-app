a2bbda8c91c3d10357530331a5a5a02de5af2731
vibek44 <kshetrivibek@gmail.com>
2026-06-11 21:16:17 +0300

    importing SetContextLink from apollo cient and using it to create new link with headers set with token when user login

--- library-frontend/src/main.jsx
+++ library-frontend/src/main.jsx
@@ -3,11 +3,20 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { ApolloClient, HttpLink, gql, InMemoryCache } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client/react";
+import { SetContextLink } from "@apollo/client/link/context";
 
+const authLink = new SetContextLink(({ headers }) => {
+  const token = localStorage.getItem("user-token");
+
+  return {
+    headers: { ...headers, authorization: token ? `Bearer ${token}` : null },
+  };
+});
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/",
+});
 const client = new ApolloClient({
-  link: new HttpLink({
-    uri: "http://localhost:4000/",
-  }),
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
